752485a30882d8fdd1a3d6f7925a9becf82cc348
matildao-pane <edlitam@hotmail.it>
2020-02-05 18:09:16 +0100

    jupyter2

--- src/game.py
+++ src/game.py
@@ -37,9 +37,9 @@ class Game():
             games_writer = csv.writer(games_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             games_writer.writerow(["id","name","descr", "genres"])
 
-            files = (listdir("./Cleaned_dataset"))
+            files = (listdir("C:/Users/Matilde/Desktop/dataminingprog/Cleaned_dataset"))
             for file in files:
-                with open("./Cleaned_dataset/"+file) as f:
+                with open("C:/Users/Matilde/Desktop/dataminingprog/Cleaned_dataset/"+file) as f:
                     gameinfo = {}
                     game = json.load(f)
                     # print(game['name'])
@@ -95,38 +95,32 @@ class Game():
                                 for w in word_tokens: 
                                     if w not in stop_words: 
                                         filtered_sentence.append(w)
-
-                                stemmer = SnowballStemmer('english')
-                                stemSentence = ''
-                                for word in filtered_sentence:
-                                    stem = stemmer.stem(word)
-                                    stemSentence += stem
-                                    stemSentence += " "
-
-                                descri = re.sub("\\b\\w{0,2}\\b|[^a-zA-Z ]"," ", str(stemSentence))
-
                                 
-                                # remove whitespaces 
-                                descri = ' '.join(descri.split())
-                                descri = descri.strip()
-
-                                
-
                                 if descri != '':
                                     # print('-'+name+'-'+descri+'-')
                                     lang = detect(descri)
                                     
                                     if lang != 'en':
                                         descri=''
-                                        print(lang)
-                                        print(f)
+                                        #print(lang)
+                                        #print(f)
+                                    else:
+                                        stemmer = SnowballStemmer('english')
+                                        stemSentence = ''
+                                        for word in filtered_sentence:
+                                            stem = stemmer.stem(word)
+                                            stemSentence += stem
+                                            stemSentence += " "
 
-                                # print(word_tokens) 
-                                # print(filtered_sentence) 
+                                        descri = re.sub("\\b\\w{0,2}\\b|[^a-zA-Z ]"," ", str(stemSentence))
+                                            # remove whitespaces 
+                                        descri = ' '.join(descri.split())
+                                        descri = descri.strip()
+
 
                                 genres= game['genres']
                                 gameinfo['genres']=[]
-                                gameinfo['genres_new']=[]
+                                #gameinfo['genres_new']=[]
                                 for genre in genres:
                                     flag=False                    
                                     gen=genre['name']
@@ -139,13 +133,13 @@ class Game():
 
                                     if gen != 'Platformer' and gen != 'Family' and gen != 'Casual' and gen != 'Indie' and gen != 'Arcade':
                                         gameinfo['genres'].append(gen)
-                                        gameinfo['genres_new'].append(gen)
-                                    else:
-                                        gameinfo['genres_new'].append(gen)
-                                        flag=True
+                                        #gameinfo['genres_new'].append(gen)
+                                    #else:
+                                        #gameinfo['genres_new'].append(gen)
+                                        #flag=True
                                     
                                     
-                                gameinfo['genres_new'] = list(dict.fromkeys(gameinfo['genres_new']))
+                                #gameinfo['genres_new'] = list(dict.fromkeys(gameinfo['genres_new']))
                                 gameinfo['genres'] = list(dict.fromkeys(gameinfo['genres']))
                                 
                         
@@ -162,16 +156,4 @@ class Game():
                                     # games_writer = csv.writer(games_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
                                     # games_writer.writerow([gameinfo['id'], name, descri, gameinfo['genres_new']])
 
-    def creacsvTot(self):
-        with open('games.csv', mode='w') as games_file:
-            games_writer = csv.writer(games_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            games_writer.writerow(["id","name","descr", "genres"])
-
-            files = (listdir("./Cleaned_dataset"))
-            for file in files:
-                with open("./Cleaned_dataset/"+file) as f:
-                    game = json.load(f)
-
-                with open('games.csv', mode= 'a', encoding='utf-8') as games_file:
-                    games_writer = csv.writer(games_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-                    games_writer.writerow([game['id'], game['name'], game['description_raw'], game['genres']])
+ 
